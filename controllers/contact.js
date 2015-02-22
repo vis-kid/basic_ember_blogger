b6f734e38412c8e36229b1ebaa0ce75411e530d7
ed wassermann <vis0vis@gmail.com>
2015-02-22 14:50:30 +0100

    Adds 2 controllers w/ actions. Just some spikes

---
+++ controllers/contact.js
@@ -0,0 +1,9 @@
+Blogger.ContactController = Ember.Controller.extend({
+	messageSent: false,
+  actions: {
+	  sendMessage: function() {
+      var message = prompt('Type your message here:');
+			this.set('messageSent', true);
+		}
+	}
+});
