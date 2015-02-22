ac2241b9b30293551e19ec640f527d05d0f62062
ed wassermann <vis0vis@gmail.com>
2015-02-22 14:16:04 +0100

    Setup for basic ember app plus few routes and templates

---
+++ router.js
@@ -0,0 +1,8 @@
+Blogger.Router.map(function() {
+  this.resource('posts', { path: '/'});
+	this.resource('about');
+	this.resource('contact', function() {
+	  this.resource('phone');
+	  this.resource('email');
+	});
+});
